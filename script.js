123ac8eff28679fcf4ebba0ad92ed20efdbf9f33
Wesley Gauntt <wesleygauntt92@gmail.com>
2016-08-30 23:13:05 -0700

    task: added overlay on mouseover. additionally, edited spacing on style.css

--- script.js
+++ script.js
@@ -26,12 +26,15 @@ function domobj(){
       if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
     }
     $("#content").append(thishtml)
+
+    //Listener function to accomodate client side rendering of products.
+    listenForProductHover();
   }
   
 }
 
 function productobj(product, i){
-  var self          = this;
+  var self = this;
   self.photo        = product.photos.medium_half
   self.title        = product.name
   self.tagline      = product.tagline
@@ -39,16 +42,35 @@ function productobj(product, i){
   self.htmlview     = ""
   self.index        = i
   self.custom_class = "col"+ ((i % 3) +1)
+  self.description  = product.description;
   
   self.updatehtml= function(){
     $.get('product-template.html', function(template){
-      self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{custom_class}', self.custom_class);
+      self.htmlview = template.replace('{image}', self.photo)
+        .replace('{title}', self.title)
+        .replace('{tagline}', self.tagline)
+        .replace('{url}', self.url)
+        .replace('{custom_class}', self.custom_class)
+        .replace('{description}', self.description);
     });
   }
 }
 
+/*
+  Task: We'd like to update the UI so that whenever you mouseover a product, an overlay comes across it with the description.
+
+  Solution: Use the jQuery hover function to listen for mouseover events. Use slideDown/slideUp to show/hide product overlay div.
+*/
+function listenForProductHover(){
+  $(".product-container").hover(function(){
+    $(this).find(".product-overlay").slideDown();
+  }, function(){
+    $(this).find(".product-overlay").slideUp();
+  });
+}
 
 var page=new domobj();
 page.getproducts('data.json');
 setTimeout("console.log('building html');page.updateproducthtml();",20);
 setTimeout("page.updatedom()",50)
+
